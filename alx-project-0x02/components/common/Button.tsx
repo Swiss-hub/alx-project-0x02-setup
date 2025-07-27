2276ea295043ba6cd74f6f923eea831f2e55a5c9
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-07-27 21:15:41 +0100

    Implementing Navigation and Layout

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+["import { type ButtonProps }", "shape", "size"] from "../../interfaces";
 
 interface ButtonProps {
   label: string;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Header from "@/components/layout/Header";
-import Button from "@/components/common/Button";
 
 export default function AboutPage() {
   return (
-    <buuton>
-      <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Header />
       <h1 className="text-3xl font-bold">About Page</h1>
       <p>This is the About page.</p>
-      </div>
-    </buuton>
+    </div>
   );
 }
