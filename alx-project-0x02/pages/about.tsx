4d3c57d2cb46cf2c208983289dab62c587c1273b
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-07-27 21:16:32 +0100

    Implementing Navigation and Layout

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 export default function AboutPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <button><div className="flex flex-col items-center justify-center min-h-screen">
       <Header />
       <h1 className="text-3xl font-bold">About Page</h1>
       <p>This is the About page.</p>
     </div>
+    </button>
   );
 }
